9adcf60f191dbcedf50266b6bb416fd5abd0a697
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-08-22 19:46:02 -0300

    Initial grid setup

--- AppleFrameworksApp/AppleFrameworksApp/FrameworkGridView.swift
+++ AppleFrameworksApp/AppleFrameworksApp/FrameworkGridView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(image: "app-clip", text: "App Clips")
+        }
+        
     }
 }
 
@@ -18,3 +26,23 @@ struct FrameworkGridView_Previews: PreviewProvider {
         FrameworkGridView()
     }
 }
+
+
+struct FrameworkTitleView: View {
+    var image: String
+    var text: String
+    var body: some View {
+        VStack {
+            Image(image)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(text)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
+
+
