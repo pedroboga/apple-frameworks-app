70105d9a360c87c78d4e451551c89ad851390bfb
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-08-23 13:38:44 -0300

    Created the navigation from main view to detail view. Added viewModel layer

---
+++ AppleFrameworksApp/AppleFrameworksApp/FrameworkGridViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkGridViewModel.swift
+//  AppleFrameworksApp
+//
+//  Created by Pedro Boga on 23/08/22.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    @Published var isShowingDetailView = false
+}
